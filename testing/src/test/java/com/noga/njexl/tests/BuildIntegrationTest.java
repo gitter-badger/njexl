6d14c79c5c275c9e3ac084f4ab790659d1b533f9
nmondal <nabarun.mondal@gmail.com>
2015-04-22 20:39:09 +0530

    Adding Copyrights!

--- testing/src/test/java/com/noga/njexl/tests/BuildIntegrationTest.java
+++ testing/src/test/java/com/noga/njexl/tests/BuildIntegrationTest.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2015 Nabarun Mondal
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.noga.njexl.tests;
 
 import com.noga.njexl.lang.extension.dataaccess.DataMatrix;
@@ -45,14 +61,14 @@ public class BuildIntegrationTest{
     @Test
     public void excelDataLoading() throws Exception {
         // now just call data matrix
-        DataMatrix matrix = DataMatrix.file2matrix(EXCEL_FILE, "Data");
+        DataMatrix matrix = DataMatrix.loc2matrix(EXCEL_FILE, "Data");
         Assert.assertNotNull(matrix);
     }
 
     @Test
     public void URIDataLoading() throws Exception {
         // now just call data matrix
-        DataMatrix matrix = DataMatrix.file2matrix(URL,"0");
+        DataMatrix matrix = DataMatrix.loc2matrix(URL,"0");
         Assert.assertNotNull(matrix);
     }
 }
